92d02fcd7ab284d4efd4c1372591a71ef87e752e
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-08 13:04:18 +0700

    feat(hero, cbir): add SVG

--- my-app/src/components/ui/hero.tsx
+++ my-app/src/components/ui/hero.tsx
@@ -3,32 +3,41 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
 const Hero = () => {
-return (
-  <div>
-    <div className="flex flex-col justify-center">
-      <h1 className="text-center text-custom-green font-outfit lg:leading-[50px] text-[83px] lg:text-6xl font-extrabold tracking-[0.54px]">
-        Reverse
-      </h1>
-      <h1 className="text-center text-custom-black font-outfit text-[83px] lg:text-6xl font-extrabold">
-        Image Search
-      </h1>
-      <p className="text-center text-custom-black font-outfit text-[15px] lg:text-s font-regular font-raleway mt-[10px]">
-        Kami dari kelompok 4 Keluarga Cemara dari kelas IF 01<br/>
-        yang beranggotakan Amel, Zaki, Angie. Project Reverse Image Search ini merupakan<br/>
-        tugas besar mata kuliah IF2123 Aljabar Linear dan Geometri.
+  return (
+    <div className="h-[475px]">
+      <Image
+        src="/home-1.svg"
+        alt="Image Input"
+        width={500}
+        height={500}
+        className="absolute left-0 -top-48 w-[500px] lg:w-[100%] -z-[1]"
+      ></Image>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-center text-custom-green font-outfit lg:leading-[50px] text-[83px] lg:text-6xl font-extrabold tracking-[0.54px]">
+          Reverse
+        </h1>
+        <h1 className="text-center text-custom-black font-outfit text-[83px] lg:text-6xl font-extrabold">
+          Image Search
+        </h1>
+        <p className="text-center text-custom-black font-outfit text-[15px] lg:text-s font-regular font-raleway mt-[10px]">
+          Kami dari kelompok 4 Keluarga Cemara dari kelas IF 01
+          <br />
+          yang beranggotakan Amel, Zaki, Angie. Project Reverse Image Search ini
+          merupakan
+          <br />
+          tugas besar mata kuliah IF2123 Aljabar Linear dan Geometri.
         </p>
-      <div className="flex justify-center">
-        <Button
-          variant="outline"
-          className="text-white bg-custom-green-dark font-semibold font-raleway mt-5 rounded-xl"
+        <div className="flex justify-center">
+          <Button
+            variant="outline"
+            className="text-white bg-custom-green-dark font-semibold font-raleway mt-5 rounded-xl"
           >
-          Explore More
-        </Button>
+            Explore More
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-  
-);
+  );
 };
 
 export default Hero;
