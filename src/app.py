6498afcd7476ed926e29ab1bec6a7e992b8c15db
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-12 09:19:27 +0700

    feat(app): comment out the unnecessary

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template,jsonify
 import os
- 
 app = Flask(__name__)
+
+# app.middleware = [
+#     'flask_cors.CORS',
+#     {'AllowedOriginsMiddleware': ('*',)},
+# ]
 
 base_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),"test")
 
@@ -13,6 +17,10 @@ DOWNLOAD_FOLDER = os.path.join(base_path,"Download")
 def get_data():
     data = {'message': 'Hello from Flask API!'}
     return jsonify(data)
+
+# @app.route('/api/cbir-color', methods=['POST'])
+# def cbir_color():
+#     return 'Hello, World!'
  
 if __name__ == '__main__':
     app.run(debug=True)
